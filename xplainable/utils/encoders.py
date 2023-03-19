b042a520fa3352616500324463ae52f0c6f28983
tim-huntley <tim.huntley12@gmail.com>
2023-03-20 02:23:22 +1000

    added utils for gui support

---
+++ xplainable/utils/encoders.py
@@ -0,0 +1,12 @@
+import numpy as np
+import json
+
+class NpEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        if isinstance(obj, np.floating):
+            return float(obj)
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return super(NpEncoder, self).default(obj)
