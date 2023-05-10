44ca54e3d816bf78743069e29dc94eee9e154c9c
tim-huntley <tim.huntley12@gmail.com>
2023-05-11 03:42:47 +1000

    added handler for df with no categories

--- xplainable/core/optimisation/targeting.py
+++ xplainable/core/optimisation/targeting.py
@@ -183,7 +183,10 @@ def generate_ruleset(
     else:
         cols = [col for col in cols if col not in num_cols]
         rule_data = rule_data[cols]
-        
+    
+    if len(cols) == 0:
+        return []
+
     dummied = pd.get_dummies(rule_data, dummy_na=False, prefix_sep='___')
 
     support_rules = {i: [u for u in rule_data[i].value_counts()[
