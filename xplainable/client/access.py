5342981bcf7f25a6d4aed049c1b1792e97e37d6e
tim-huntley <tim.huntley12@gmail.com>
2023-05-11 03:39:57 +1000

    refactored to keyring

--- xplainable/client/access.py
+++ xplainable/client/access.py
@@ -4,61 +4,24 @@ from IPython.display import clear_output
 import os
 import sys
 from getpass import getpass
-from dotenv import dotenv_values
 import warnings
 import logging
 from ..gui.components import KeyValueTable, Header
 from IPython.display import display, clear_output
-
-
-def create_file_if_not_exisits():
-    pass
-
-def store_api_key(api_key):
-    try:
-        env_file_dir = os.path.join(xplainable.__path__[0], '.env')
-        if not os.path.isdir(env_file_dir):
-            os.makedirs(env_file_dir)
-        
-        env_file_path = os.path.join(env_file_dir, '.api_key')
-        
-        with open(env_file_path, 'w') as env_file:
-            env_file.write(f'XPLAINABLE_API_KEY={api_key}\n')
-
-        logging.info(f"Stored API Key to {env_file_path}")
-    except Exception as e:
-        warnings.warn("Could not store API key. Check documentation.")
-        logging.info(f"Could not store API key to {env_file_path}")
-
-def get_api_key():
-    # Determine the path to the .xp.env file
-    try:
-        env_file_path = os.path.join(xplainable.__path__[0], '.env', '.api_key')
-
-        api_key = dotenv_values(env_file_path).get('XPLAINABLE_API_KEY')
-        
-        if api_key:
-            logging.info(f"Retreived API Key from {env_file_path}")
-            return api_key
-        else:
-            logging.info(f"Could not retreive API key from {env_file_path}")
-            warnings.warn("Could not retreive API key. Check documentation.")
-    
-    except:
-        return False
+import keyring
 
 
 def initialise():
     
     has_set = False
-    api_key = get_api_key()
+    api_key = keyring.get_password('XPLAINABLE', 'api_key')
     if not api_key:
         api_key = getpass("Paste a valid API Key: ")
         has_set = True
     
     try:
         xplainable.client = Client(api_key)
-        store_api_key(api_key)
+        keyring.set_password('XPLAINABLE', 'api_key', api_key)
         clear_output()
 
         pyinf = sys.version_info
@@ -97,5 +60,5 @@ def initialise():
         api_key = getpass(text)
         clear_output()
         xplainable.client = Client(api_key)
-        store_api_key(api_key)
+        keyring.set_password('XPLAINABLE', 'api_key', api_key)
         return "Initialised"
