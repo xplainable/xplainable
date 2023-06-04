11b92885ae8aea25581d85e61bc4c9b8cc10154e
tim-huntley <tim.huntley12@gmail.com>
2023-06-04 22:12:14 +1000

    implemented tornado version check

--- xplainable/__init__.py
+++ xplainable/__init__.py
@@ -1,7 +1,11 @@
 from .client.client import Client
 from .gui import *
 from .client.init import initialise, reinitialise
+from .utils.handlers import check_critical_versions
 import warnings
+
+# Ensure critical dependency versions match
+check_critical_versions()
 
 __author__ = 'xplainable pty ltd'
 from ._version import __version__
