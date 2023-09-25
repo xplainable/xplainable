002ad802de5a251639bf7be24895bb37464d18be
tim-huntley <tim.huntley12@gmail.com>
2023-09-25 11:06:18 +1000

    added max_features param to gpt report generator

--- xplainable/gpt/report.py
+++ xplainable/gpt/report.py
@@ -1,6 +1,23 @@
 import xplainable as xp
 
-def gpt_explainer(model_id, version_id, target_info='', other_details=''):
+def gpt_explainer(
+        model_id, version_id, target_info='', project_objective='',
+        max_features=15
+        ):
+    """
+    Generates a report for a given model and version.
+
+    Args:
+        model_id (str): The model ID.
+        version_id (str): The version ID.
+        target_info (str): The target information.
+        project_objective (str): The project objective.
+        max_features (int): The maximum number of features to analyse.
+
+    Returns:
+        str: The report.
+
+    """
 
     if xp.client is None:
         raise ValueError(
@@ -8,6 +25,6 @@ def gpt_explainer(model_id, version_id, target_info='', other_details=''):
                 from None
 
     report = xp.client._gpt_report(
-        model_id, version_id, target_info, other_details)
+        model_id, version_id, target_info, project_objective, max_features)
     
     return report
